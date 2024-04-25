3f18005bd69e7b4e1dae2a6e0cbb5c6390998a2d
Tadiwa Chipolopolo <tadiwanashenicckky@gmail.com>
2024-04-26 00:41:07 +0200

    added the last 3 components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Imgmobile from "../images/image-web-3-mobile.jpg";
 import { Box, Button, Stack, Typography } from "@mui/material";
+import imgretro from "../images/image-retro-pcs.jpg";
+import toplaptop from "../images/image-top-laptops.jpg";
+import imggaming from "../images/image-gaming-growth.jpg";
 
 const Home = () => {
   return (
@@ -22,7 +25,7 @@ const Home = () => {
         The Bright Future of Web 3.0?
       </Typography>
 
-      <Typography sx={{ fontSize:"1rem", px: 3 }}>
+      <Typography sx={{ fontSize: "1rem", px: 3 }}>
         We dive into the next evolution of the web that claims to put the power
         of the platforms back into the hands of the people. But is it really
         fulfilling its promise?
@@ -53,14 +56,14 @@ const Home = () => {
       >
         <h1>New</h1>
         <br />
-      
+
         <Typography sx={{ color: "hsl(36, 100%, 99%)", py: 3 }}>
-          <h1 style={{fontSize:"1.5rem"}}>Hydrogen VS Electric Cars</h1>
+          <h1 style={{ fontSize: "1.5rem" }}>Hydrogen VS Electric Cars</h1>
           <p> Will hydrogen-fueled cars ever catch up to EVs?</p>
         </Typography>
         <hr />
         <Typography sx={{ color: "hsl(36, 100%, 99%)", py: 3 }}>
-          <h1 style={{fontSize:"1.5rem"}}>The Downsides of AI Artistry</h1>
+          <h1 style={{ fontSize: "1.5rem" }}>The Downsides of AI Artistry</h1>
           <p>
             {" "}
             What are the possible adverse effects of on-demand AI image
@@ -69,7 +72,7 @@ const Home = () => {
         </Typography>
         <hr />
         <Typography sx={{ color: "hsl(36, 100%, 99%)", py: 3 }}>
-          <h1 style={{fontSize:"1.5rem"}}>Is VC Funding Drying Up?</h1>
+          <h1 style={{ fontSize: "1.5rem" }}>Is VC Funding Drying Up?</h1>
           <p>
             {" "}
             Private funding by VC firms is down 50% YOY. We take a look at what
@@ -77,8 +80,32 @@ const Home = () => {
           </p>
         </Typography>
       </Box>
-      <Stack>
-        <Box></Box>
+      <Stack sx={{ flex: "flex-wrap" }}>
+        <Box sx={{ px: 3, py: 3, display: "inline-flex", gap: 3 }}>
+          <img src={imgretro} alt="" width={160} />
+          <Typography>
+            <h1 style={{ color: "hsl(233, 8%, 79%)" }}>01</h1>
+            <h2>Reviving Retro PCs</h2>{" "}
+            <p style={{ color: "hsl(233, 8%, 79%)" }}>What happens when old PCs are given modern upgrades?</p>
+          </Typography>
+        </Box>
+        <Box sx={{ px: 3, py: 3, display: "inline-flex", gap: 3 }}>
+          <img src={toplaptop} alt="" width={160} />
+          <Typography>
+            {" "}
+            <h1 style={{ color: "hsl(233, 8%, 79%)" }}>02</h1>{" "}
+            <h2>Top 10 Laptops of 2022 </h2>{" "}
+            <p style={{ color: "hsl(233, 8%, 79%)" }}>Our best picks for various needs and budgets.</p>
+          </Typography>
+        </Box>
+        <Box sx={{ px: 3, py: 3, display: "inline-flex", gap: 3 }}>
+          <img src={imggaming} alt="" width={160} />
+          <Typography>
+            <h1 style={{ color: "hsl(233, 8%, 79%)" }}>03</h1>{" "}
+            <h2>The Growth of Gaming</h2>{" "}
+            <p style={{ color: "hsl(233, 8%, 79%)" }}>How the pandemic has sparked fresh opportunities.</p>
+          </Typography>
+        </Box>
       </Stack>
     </div>
   );
